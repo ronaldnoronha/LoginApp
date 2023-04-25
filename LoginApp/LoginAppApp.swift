7bae5470a92b964fd35fd494a8bb3ac611bd10f0
Ronald Noronha <ronald.noronha@cba.com.au>
2023-04-25 16:24:42 +1000

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Noronha, Ronald on 25/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
