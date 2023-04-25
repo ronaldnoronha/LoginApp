5c467f01c501f041d3b8ef8ce2d043f7118fa459
Ronald Noronha <ronald.noronha@cba.com.au>
2023-04-25 23:56:24 +1000

    Initial Commit

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -7,20 +7,57 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    let country: String
+}
+
+class LoginManager: ObservableObject {
+    @Published var isLoggedIn = false
+    @State var country: String?
+    
+    func login() async throws {
+        let url = URL(string: "http://130.41.233.29:8080/login?name=ron&password=hey")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let result = try JSONDecoder().decode(Response.self, from: data)
+        country = result.country
+    }
+}
+
+
 struct ContentView: View {
+    @StateObject var loginManager = LoginManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if let country = loginManager.country, loginManager.isLoggedIn {
+            LoggedInView(country: country)
+        } else {
+            LoginView(loginManager: loginManager)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct LoginView: View {
+    @ObservedObject var loginManager : LoginManager
+    
+    var body: some View {
+        Button("login") {
+            Task {
+                try await loginManager.login()
+            }
+        }
+    }
+}
+
+struct LoggedInView: View {
+    var country: String
+    var body: some View {
+        Text("\(country.capitalized)")
     }
 }
